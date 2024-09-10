3e9e2946fd3c3c6b55e02e2316dd353dca78dba0
akleinvm <akleinvm@gmail.com>
2024-09-10 18:07:06 +0800

    Added styles and functions

--- UploadButton/index.ts
+++ UploadButton/index.ts
@@ -1,53 +1,67 @@
 import { IInputs, IOutputs } from "./generated/ManifestTypes";
 
 export class UploadButton implements ComponentFramework.StandardControl<IInputs, IOutputs> {
+    private context: ComponentFramework.Context<IInputs>;
+    private notifyOutputChanged: () => void;
+    private container: HTMLDivElement;
+    private button: HTMLButtonElement;
+    private fileInput: HTMLInputElement;
+    private base64Output: string;
+    private fileName: string;
 
-    /**
-     * Empty constructor.
-     */
-    constructor()
-    {
+    constructor(){}
 
-    }
-
-    /**
-     * Used to initialize the control instance. Controls can kick off remote server calls and other initialization actions here.
-     * Data-set values are not initialized here, use updateView.
-     * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to property names defined in the manifest, as well as utility functions.
-     * @param notifyOutputChanged A callback method to alert the framework that the control has new outputs ready to be retrieved asynchronously.
-     * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
-     * @param container If a control is marked control-type='standard', it will receive an empty div element within which it can render its content.
-     */
     public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container:HTMLDivElement): void
     {
-        // Add control initialization code
+        this.context = context;
+        this.notifyOutputChanged = notifyOutputChanged;
+        this.container = container;
+
+        this.button = document.createElement('button');
+        this.button.className = 'default-button';
+        this.button.innerText = this.context.parameters.Text.raw ?? 'Upload file';
+        this.button.addEventListener('click', () => document.getElementById('fileInput')?.click());
+        this.container.appendChild(this.button);
+
+        this.fileInput = document.createElement('input');
+        this.fileInput.type = 'file';
+        this.fileInput.accept = this.context.parameters.Accept.raw ?? '.txt';
+        this.fileInput.id = 'fileInput';
+        this.fileInput.addEventListener('change', this.handleFileUpload.bind(this));
+        this.container.appendChild(this.fileInput);
     }
 
-
-    /**
-     * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
-     * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
-     */
     public updateView(context: ComponentFramework.Context<IInputs>): void
     {
-        // Add code to update control view
+        this.context = context;
+        this.button.innerText = this.context.parameters.Text.raw ?? 'Upload file';
+        this.fileInput.accept = this.context.parameters.Accept.raw ?? '.txt';
     }
 
-    /**
-     * It is called by the framework prior to a control receiving new data.
-     * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as "bound" or "output"
-     */
     public getOutputs(): IOutputs
     {
-        return {};
+        return {
+            FileName: this.fileName,
+            FileContent: this.base64Output
+        };
     }
 
-    /**
-     * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
-     * i.e. cancelling any pending remote calls, removing listeners, etc.
-     */
     public destroy(): void
     {
         // Add code to cleanup control if necessary
+    }
+
+    private handleFileUpload (event: Event): void {
+        const file = (event.target as HTMLInputElement).files?.[0];
+        const filename = file?.name ?? '';
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = (file) => {
+                this.fileName = filename;
+                this.base64Output = reader.result?.toString() ?? '';
+                this.notifyOutputChanged();
+            }
+            reader.readAsDataURL(file)
+        }
     }
 }
